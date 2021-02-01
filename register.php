05ef5c0f77d2915c953af5542e49a6420b4cb5ee
masadkhanek <masadkhanek@gmail.com>
2021-02-01 13:41:52 +0500

    make register page

---
+++ register.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>welcome to spotify</title>
+  </head>
+  <body>
+    <div class="inputContainer">
+      <form class="loginForm" action="register.php" method="post">
+      <h2>login to your account</h2>
+      <input type="loginUsername" name="loginUsername" value="text"placeholder="e.g.asadkhan">
+      </form>
+
+    </div>
+
+
+  </body>
+</html>
