7816d1228b10a3e9e4e129a7f093197aebe7d8c1
masadkhanek <masadkhanek@gmail.com>
2021-02-01 14:28:08 +0500

    some more changes

--- register.php
+++ register.php
@@ -8,10 +8,10 @@
     <div class="inputContainer">
       <form class="loginForm" action="register.php" method="post">
       <h2>login to your account</h2>
-      <label for="loginUsername">Username</label>
-      <p><input type="loginUsername" name="loginUsername" value="text"placeholder="e.g.asadkhan"></p>
-      <label for="loginpassword">password</label>
-      <p><input type="password" name="password" value="password"placeholder="Your Password"></p>
+      <p><label for="loginUsername">Username</label>
+      <input type="loginUsername" name="loginUsername" value="text"placeholder="e.g.asadkhan"></p>
+    <p>  <label for="loginpassword">password</label>
+      <input type="password" name="password" value="password"placeholder="Your Password"></p>
     <button type="submit" name="loginButton">LOG IN</button>
       </form>
 
@@ -22,25 +22,25 @@
       <h2>Create your account</h2>
 
 
-      <p><label for="firstName">First Name</label></p>
-        <p><input type="firstName" name="firstName" value="text"placeholder="e.g.asadkhan"></p>
+      <p><label for="firstName">First Name</label>
+        <input type="firstName" name="firstName" value="text"placeholder="e.g.asadkhan"></p>
 
-        <p><label for="lastname">last Name</label></p>
-          <p><input type="lastname" name="lastname" value="text"placeholder="e.g.asadkhan"></p>
+        <p><label for="lastname">last Name</label>
+          <input type="lastname" name="lastname" value="text"placeholder="e.g.asadkhan"></p>
 
-          <p><label for="email">Email</label></p>
-            <p><input type="email" name="email" value="text"placeholder="e.g.asadkhan"></p>
+          <p><label for="email">Email</label>
+          <input type="email" name="email" value="text"placeholder="e.g.asadkhan"></p>
 
-            <p><label for="email2">Conform Email</label></p>
-              <p><input type="email2" name="email2" value="text"placeholder="e.g.asadkhan"></p>
+            <p><label for="email2">Conform Email</label>
+            <input type="email2" name="email2" value="text"placeholder="e.g.asadkhan"></p>
 
-              <p><label for="loginPassword">login Password</label></p>
-                <p><input type="loginPassword" name="loginPassword" value="password"placeholder="Your Password"></p>
+              <p><label for="loginPassword">login Password</label>
+              <input type="loginPassword" name="loginPassword" value="password"placeholder="Your Password"></p>
 
 
 
-      <label for="password2">conform password</label>
-      <p><input type="password2" name="password2" value="Password"placeholder="Your Password"></p>
+      <p><label for="password2">conform password</label>
+      <input type="password2" name="password2" value="Password"placeholder="Your Password"></p>
       <button type="submit" name="registerButton">SIGN UP</button>
       </form>
     </div>
